127991fb2d92963b8d2bfbbd72c27249eb3cd7f1
Ahsanmusht <ahsanmushtaq901@gmail.com>
2024-08-30 18:59:07 +0500

    Some Code

---
+++ database/mysql.js
@@ -0,0 +1,22 @@
+const mysql = require('mysql');
+
+module.exports = class Mysql {
+  static connect() {
+    // establish connection
+    const db = mysql.createConnection({
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: 'skahme5_skdigitech',
+      port:3306
+    });
+    // connect to database
+    db.connect((err) => {
+      if (err) {
+        throw err;
+      }
+      console.log('Mysql Connected');
+    });
+    return db;
+  }
+};
